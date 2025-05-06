8a75242e76cca04a07d7c8f68b16ff307bd6997a
chocolaggibbiddori <didrkd684@naver.com>
2025-05-06 16:52:37 +0900

    [Section 3] 스프링 배치 시작 - Hello Spring Batch 시작하기

---
+++ src/main/java/chocola/springbatch/HelloJobConfiguration.java
@@ -0,0 +1,49 @@
+package chocola.springbatch;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class HelloJobConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job helloJob() {
+        return jobBuilderFactory
+                .get("helloJob")
+                .start(helloFirstStep())
+                .next(helloSecondStep())
+                .build();
+    }
+
+    private Step helloFirstStep() {
+        return stepBuilderFactory
+                .get("helloFirstStep")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info(">>> Hello Spring Batch First Step <<<");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    private Step helloSecondStep() {
+        return stepBuilderFactory
+                .get("helloSecondStep")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info(">>> Hello Spring Batch Second Step <<<");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+}
