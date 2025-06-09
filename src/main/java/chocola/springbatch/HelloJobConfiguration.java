1f56ab8774246c7bda6c4e04ed30582ef6cfb507
chocolaggibbiddori <didrkd684@naver.com>
2025-06-09 22:42:44 +0900

    [Section 6] 스프링 배치 실행 - Step - JobStep

--- src/main/java/chocola/springbatch/HelloJobConfiguration.java
+++ src/main/java/chocola/springbatch/HelloJobConfiguration.java
@@ -2,10 +2,17 @@ package chocola.springbatch;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.StepExecutionListener;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.step.job.DefaultJobParametersExtractor;
+import org.springframework.batch.core.step.job.JobParametersExtractor;
+import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,29 +23,58 @@ public class HelloJobConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
+    private final JobLauncher jobLauncher;
 
     @Bean
     public Job helloJob() {
-        return jobBuilderFactory
-                .get("helloJob")
-                .start(step1())
+        return jobBuilderFactory.get("helloJob")
+                .start(jotStep())
                 .next(step2())
+                .build();
+    }
+
+    private Step jotStep() {
+        return stepBuilderFactory.get("jotStep")
+                .job(childjob())
+                .launcher(jobLauncher)
+                .parametersExtractor(jobParametersExtractor())
+                .listener(new StepExecutionListener() {
+                    @Override
+                    public void beforeStep(StepExecution stepExecution) {
+                        stepExecution.getExecutionContext().putString("name", "user1");
+                    }
+
+                    @Override
+                    public ExitStatus afterStep(StepExecution stepExecution) {
+                        return null;
+                    }
+                })
+                .build();
+    }
+
+    private Job childjob() {
+        return jobBuilderFactory.get("childJob")
+                .start(step1())
                 .build();
     }
 
     private Step step1() {
         return stepBuilderFactory.get("step1")
-                .tasklet(new CustomTasklet())
-                .startLimit(5)
-                .allowStartIfComplete(true)
+                .tasklet((contribution, chunkContext) -> RepeatStatus.FINISHED)
                 .build();
+    }
+
+    private JobParametersExtractor jobParametersExtractor() {
+        DefaultJobParametersExtractor extractor = new DefaultJobParametersExtractor();
+        extractor.setKeys(new String[]{"name"});
+        return extractor;
     }
 
     private Step step2() {
         return stepBuilderFactory.get("step2")
                 .tasklet((contribution, chunkContext) -> {
                     System.out.println("step 2");
-                    throw new RuntimeException("error");
+                    return RepeatStatus.FINISHED;
                 })
                 .startLimit(3)
                 .build();
